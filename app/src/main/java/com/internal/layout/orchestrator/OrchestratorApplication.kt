59e003ede1e7f5c5cadd456a863f3f8ddfbf9c54
mattmhown <accounts@hown.com.au>
2025-09-15 16:32:12 +1000

    Create OrchestratorApplication.kt

---
+++ app/src/main/java/com/internal/layout/orchestrator/OrchestratorApplication.kt
@@ -0,0 +1,9 @@
+package com.internal.layout.orchestrator
+
+import android.app.Application
+
+class OrchestratorApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
